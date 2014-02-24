861b10dffeca29e2520dc0eb8991a1a28a7ecde2
Huw Taylor <huw_taylor@hotmail.co.uk>
2014-02-24 17:29:35 +0000

    added network event handler

--- src/jog/network.java
+++ src/jog/network.java
@@ -1,7 +1,25 @@
 package jog;
 
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
 public abstract class network {
 	
+	public interface NetworkEventHandler {
+		
+//		public void onConnect(Peer peer);
+//		public void onDisconnect(Peer peer);
+//		public void onMessage(Peer peer, String message);
+		
+	}
 	
+	public class Server {
+		
+	}
+	
+	public class Client {
+		
+	}
 
 }
