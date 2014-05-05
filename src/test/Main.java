fd5bc288567d743e6362f1cc053821310805b1d4
IMP1 <IMP1@mail.com>
2014-05-05 22:15:27 +0100

    added tests for newer features

--- src/test/Main.java
+++ src/test/Main.java
@@ -38,8 +38,8 @@ public class Main implements jog.input.InputEventHandler, jog.network.ClientEven
 	}
 	
 	private void start() {
-		jog.window.initialise(TITLE, WIDTH, HEIGHT, FPS, jog.window.WindowMode.WINDOWED);
-//		jog.window.setFullscreen(true);
+		jog.window.initialise(TITLE, WIDTH, HEIGHT, FPS);
+		jog.window.setResizable(true);
 		jog.graphics.initialise();
 		img = jog.image.newImage("src/test/gfx/ship.png");
 		jog.filesystem.addLocation("src/test/gfx");
@@ -82,11 +82,19 @@ public class Main implements jog.input.InputEventHandler, jog.network.ClientEven
 		int length = 32;
 		jog.graphics.setColour(0, 0, 0);
 		jog.graphics.circle(true, 128, 64 + Math.sin(Math.PI / 3) * length / 1.5, 64);
-		drawCircles(0, length, jog.graphics.BlendMode.ADDITIVE);
+		drawCircles(128 - 128, length, jog.graphics.BlendMode.ALPHA);
+		
+		jog.graphics.setColour(0, 0, 0);
+		jog.graphics.circle(true, 344, 64 + Math.sin(Math.PI / 3) * length / 1.5, 64);
+		drawCircles(344 - 128, length, jog.graphics.BlendMode.ADDITIVE);
 		
 		jog.graphics.setColour(255, 255, 255);
-		jog.graphics.circle(true, 344, 64 + Math.sin(Math.PI / 3) * length / 1.5, 64);
-		drawCircles(344 - 128, length, jog.graphics.BlendMode.SUBTRACTIVE);
+		jog.graphics.circle(true, 560, 64 + Math.sin(Math.PI / 3) * length / 1.5, 64);
+		drawCircles(560 - 128, length, jog.graphics.BlendMode.SUBTRACTIVE);
+		
+		jog.graphics.setColour(255, 255, 255);
+		jog.graphics.circle(true, 776, 64 + Math.sin(Math.PI / 3) * length / 1.5, 64);
+		drawCircles(776 - 128, length, jog.graphics.BlendMode.MULTIPLICATIVE);
 		
 		jog.graphics.setColour(255, 255, 255);
 		jog.graphics.push();
@@ -98,48 +106,38 @@ public class Main implements jog.input.InputEventHandler, jog.network.ClientEven
 		
 		jog.graphics.draw(img, 460, 344, rotation, 35, 48, 1, 1);
 		jog.graphics.draw(img, 352, 344, -rotation, 35, 48, 1, 1);
-		
-		int triangleX = 192;
-		int triangleY = 256;
-		length = 256;
-		double radius;
-		int direction;
-		for (int i = 0; i < 5; i ++) {
-			jog.graphics.setColour(255 / (i+1), 255 / (i+1), 255 / (i+1));
-			direction = (i % 2) * 2 - 1;
-			radius = length * (Math.sqrt(3) / 7.0);
-			int x1 = triangleX;
-			int x2 = triangleX - length / 2;
-			int x3 = triangleX + length / 2;
-			int y1 = (int) (triangleY - direction * (Math.sin(Math.PI / 3) * length - radius));
-			int y2 = (int) (triangleY + direction * Math.sin(Math.PI / 6) * radius);
-			int y3 = (int) (triangleY + direction * Math.sin(Math.PI / 6) * radius);
-			jog.graphics.triangle(true, x1, y1, x2, y2, x3, y3);
-			triangleY -= direction * radius * 6 / 8;
-			length = (int) (radius * 2);
-		}
+	
+		int scissorY = 256;
+		jog.graphics.setScissor(32, scissorY, 128, 128);
+		jog.graphics.setColour(255, 255, 255);
+		jog.graphics.rectangle(true, 32, scissorY, 256, 256);
+		jog.graphics.setScissor();
 		
 		length = 64;
 		jog.graphics.push();
-		jog.graphics.translate(256, -224);
+		jog.graphics.translate(640, 224);
 		jog.graphics.polygon(true, -length, 0, -length * Math.sin(Math.PI/6), -length * Math.cos(Math.PI/6), length * Math.sin(Math.PI/6), -length * Math.cos(Math.PI/6), 
 								length, 0,  length * Math.sin(Math.PI/6), length * Math.cos(Math.PI/6), -length * Math.sin(Math.PI/6), length * Math.cos(Math.PI/6));
 		jog.graphics.pop();
 		
+		
+		
 		jog.graphics.rectangle(false, 8, 32, 4, 128);
 		
 		jog.graphics.setColour(255, 255, 255);
-		jog.graphics.print(multiplayerRole, 0, 12);
-		if (multiplayerRole == "Server") {
+		
+		if (multiplayerRole == "Server" && server.connected()) {
+			jog.graphics.print(multiplayerRole, 0, 12);
 			jog.graphics.print(server.getAddress() + ":" + server.getPort(), 0, 24);
 			String[] lines = server.getClients();
 			for (int i = 0; i < lines.length; i ++) {
 				jog.graphics.print(lines[i], 8, 36 + i * 12);
 			}
+		} else if (multiplayerRole == "Client") {
+			jog.graphics.print(multiplayerRole, 0, 12);
 		}
 		
 		jog.graphics.rectangle(true, 640, 480, 128, 64);
-		jog.graphics.rectangle(true, 700, 32, 8, 256);
 		
 		//--------------------------------\\
 		jog.graphics.setColour(255, 255, 255);
